d6c322a3b2ec78dfefb8197e8d5e98e0f296ba18
yone64 <yoneyan64@msn.com>
2017-03-31 01:49:05 +0900

    拡張メソッド追加

---
+++ src/BasicLibrary.Pcl/Extensions/StringEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLibrary.Pcl.Extensions
+{
+    /// <summary>
+    /// Stringに対する拡張メソッド集
+    /// </summary>
+    public static class StringEx
+    {
+        /// <summary>
+        /// 指定された文字列が null または System.String.Empty 文字列であるかどうかを示します。
+        /// </summary>
+        /// <param name="s">テストする文字列。</param>
+        /// <returns>パラメーターが null または空の文字列 ("") の場合は true。それ以外の場合は false。</returns>
+        public static bool IsNullOrEmpty(this string s) => string.IsNullOrEmpty(s);
+    }
+}
