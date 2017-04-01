eaba16ab28376846d6803ab0d6ba692dc0ad7ac7
yone64 <yoneyan64@msn.com>
2017-04-01 17:41:45 +0900

    TestProjectの追加

---
+++ test/BasicLibrary.Pcl.Tests/Extensions/StringExTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicLibrary.Pcl.Extensions
+{
+    [TestClass]
+    public class StringExTest
+    {
+        [TestMethod]
+        public void IsNullOrEmptyTest()
+        {
+            string source = "a";
+            source.IsNullOrEmpty().IsFalse();
+
+            source = "";
+            source.IsNullOrEmpty().IsTrue();
+
+            source = null;
+            source.IsNullOrEmpty().IsTrue();
+        }
+    }
+}
---
+++ test/BasicLibrary.Pcl.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicLibrary.Pcl.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicLibrary.Pcl.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a4fc7de-14fa-4c15-9881-452548c5da02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
