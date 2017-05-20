711836067a589d43100053a65cc2c8461ca9329a
yone64 <yoneyan64@msn.com>
2017-05-20 16:51:44 +0900

    Invokeメソッド追加

---
+++ src/BasicLibrary.Pcl/Extensions/ObjectEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLibrary.Pcl.Extensions
+{
+    public static class ObjectEx
+    {
+        /// <summary>
+        /// Actionを実行します。
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="action"></param>
+        public static void Invoke<T>(this T obj, Action<T> action) => action(obj);
+
+        /// <summary>
+        /// Funcを実行します。
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="V"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="func"></param>
+        public static V Invoke<T, V>(this T obj, Func<T, V> func) => func(obj);
+
+    }
+}
---
+++ test/BasicLibrary.Pcl.Tests/Extensions/ObjectExTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicLibrary.Pcl.Extensions
+{
+    [TestClass]
+    public class ObjectExTest
+    {
+        [TestMethod]
+        public void NotNullThenTest()
+        {
+            string str = null;
+
+            str?.Invoke(s => Console.WriteLine(s));
+            
+            int sum = 0;
+            int? a = null;
+            int? b = 1;
+
+            a?.Invoke(i => sum =+ i);
+            b?.Invoke(i => sum = +i);
+            
+            //if (a != null) sum += a.Value;
+            //if (a is int i) sum += i;
+            sum.Is(1);
+        }
+    }
+}
